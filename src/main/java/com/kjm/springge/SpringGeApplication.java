ac390575fa1dcb83583d4aaae392576d45e50505
zmfflras <zmfflras@gmail.com>
2022-06-02 09:57:57 +0900

    first springGe

---
+++ src/main/java/com/kjm/springge/SpringGeApplication.java
@@ -0,0 +1,13 @@
+package com.kjm.springge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringGeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kjm/springge/SpringGeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kjm.springge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
